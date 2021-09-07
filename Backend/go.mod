2372557d8ec53ac5c6069c1c75dd273398bc358a
Sanket Wable <sanketwable@Sankets-MacBook-Pro.local>
2021-09-07 23:17:46 +0530

    adding files

---
+++ Backend/go.mod
@@ -0,0 +1,5 @@
+module ambulancedatatransmissioniot
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
---
+++ Backend/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Starting Server : 8080")
+	r := mux.NewRouter()
+	r.HandleFunc("/heartrate", homeHandler).Methods(http.MethodGet)
+	http.ListenAndServe(":8080", r)
+
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	start := r.URL.Query().Get("xstart")
+	strt, err := strconv.Atoi(start)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var r1 [][]int
+
+	for i := strt; i < strt+10; i++ {
+		x1 := rand.NewSource(time.Now().UnixNano())
+		y := rand.New(x1).Intn(200) % 10 +	50
+		row := []int{i, y}
+		r1 = append(r1, row)
+	}
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Header().Add("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+
+	json.NewEncoder(w).Encode(r1)
+	w.WriteHeader(100)
+
+}
