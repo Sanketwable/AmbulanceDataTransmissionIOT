4afcc68dafa0e2b5968727c99d588a503d9b89e2
Sanket Wable <sanketwable@Sankets-MacBook-Pro.local>
2021-09-08 15:53:58 +0530

    adding graphs

--- Backend/main.go
+++ Backend/main.go
@@ -7,19 +7,20 @@ import (
 	"net/http"
 	"strconv"
 	"time"
-
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Starting Server : 8080")
 	r := mux.NewRouter()
-	r.HandleFunc("/heartrate", homeHandler).Methods(http.MethodGet)
+	r.HandleFunc("/heartrate", HeartRateHandler).Methods(http.MethodGet)
+	r.HandleFunc("/bloodpressure", BloodPressureHandler).Methods(http.MethodGet)
 	http.ListenAndServe(":8080", r)
 
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+// HeartRateHandler is a func
+func HeartRateHandler(w http.ResponseWriter, r *http.Request) {
 
 	start := r.URL.Query().Get("xstart")
 	strt, err := strconv.Atoi(start)
@@ -35,7 +36,33 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		r1 = append(r1, row)
 	}
 	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-	w.Header().Add("Connection", "keep-alive")
+	// w.Header().Add("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+
+	json.NewEncoder(w).Encode(r1)
+	w.WriteHeader(100)
+
+}
+func BloodPressureHandler(w http.ResponseWriter, r *http.Request) {
+	// time.Sleep(time.Millisecond*3000)
+
+	start := r.URL.Query().Get("xstart")
+	strt, err := strconv.Atoi(start)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var r1 [][]int
+
+	for i := strt; i < strt+10; i++ {
+		x1 := rand.NewSource(time.Now().UnixNano())
+		systolic := rand.New(x1).Intn(200) % 20 +	130
+		diastolic := rand.New(x1).Intn(200) % 20 +	50
+		row := []int{i, systolic, diastolic}
+		r1 = append(r1, row)
+	}
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	// w.Header().Add("Connection", "keep-alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET")
 
---
+++ Frontend/js/index.js
@@ -0,0 +1,207 @@
+// Map to store AmbulanceID and AmbulanceNumber
+const AmbulanceInfo = new Map();
+AmbulanceInfo.set(1, "MH01 A5003")
+AmbulanceInfo.set(2, "MH06 G1463")
+AmbulanceInfo.set(3, "MH08 E0091")
+var selectedAmbulance = 1;
+
+// Function to show MAP
+function initMap() {
+    const uluru = { lat: -25.344, lng: 131.036 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 7,
+      center: uluru,
+    });
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+}
+
+// SelectAmbulance is selecting ambulance from drop down
+function SelectAmbulance() {
+    var e = document.getElementById("Ambulance");
+    var selectedAmbulance = e.options[e.selectedIndex].value;
+	document.getElementById("AmbulanceINFO").innerHTML = "Ambulance " + selectedAmbulance + " : " +AmbulanceInfo.get(parseInt(selectedAmbulance));
+}
+
+window.onload = function() {
+
+    var HeartBeatPoints = [];
+    var systolicBP = [];
+    var avgsystolicBP = [];
+    var averageheartrate = [];
+    var diastolicBP = [];
+    var avgdiastolicBP = [];
+
+    var heartRateChart = new CanvasJS.Chart("hearRateMonitor", {
+
+	    theme: "light2",
+	    animationEnabled: true,
+	    title: {
+		    text: "HeartBeat Data",
+	    },
+        axisY: { 
+		    title: "Rate",
+		    suffix: " bpm",
+		    crosshair: {
+			    enabled: true
+		    }
+	    },
+        axisX: { 
+		    title: "Time",
+		    suffix: " sec",
+		    crosshair: {
+			    enabled: true,
+			    snapToDataPoint: true
+		    }
+	    },
+	    toolTip:{
+		    shared:true
+	    },  
+	    data: [{
+		    type: "line",
+		    showInLegend: true,
+		    name: "Bpm",
+		    markerType: "circle",
+		    color: "#F08080",
+		    dataPoints: HeartBeatPoints,
+	    },{
+		    type: "line",
+		    showInLegend: true,
+            markerSize: 0,
+		    name: "average Bpm",
+		    lineDashType: "dash",
+		    dataPoints: averageheartrate,
+	    }]
+    });
+    updateHeartRateData();
+
+    var bloodpressureChart = new CanvasJS.Chart("bloodPressureMonitor", {
+	    theme: "light2",
+        animationEnabled: true,
+	    title: {
+		    text: "BloodPressure Data"
+	    },
+        axisY: { 
+		    title: "Hg",
+		    suffix: "mm",
+		    crosshair: {
+			    enabled: true
+		    }
+	    },
+        axisX: { 
+		    title: "Time",
+		    suffix: " sec",
+		    crosshair: {
+			    enabled: true,
+			    snapToDataPoint: true
+		    }
+	    },
+	    toolTip:{
+		    shared:true
+	    }, 
+	    data: [{
+		    type: "line",
+            showInLegend: true,
+            markerType: "square",
+            name: "Systolic",
+            color: "#F08080",
+		    dataPoints: systolicBP
+	    },{
+		    type: "line",
+		    showInLegend: true,
+		    name: "Diastolic",
+		    lineDashType: "line",
+		    dataPoints: diastolicBP,
+	    },{
+		    type: "line",
+		    showInLegend: true,
+            markerSize: 0,
+		    name: "average Systolic",
+		    lineDashType: "dash",
+		    dataPoints: avgsystolicBP,
+	    },{
+		    type: "line",
+		    showInLegend: true,
+            markerSize: 0,
+		    name: "average Diastolic",
+		    lineDashType: "dash",
+		    dataPoints: avgdiastolicBP,
+	    }]
+    });
+    updateBloodPressureData();
+
+// Initial Values
+    var xValueHR = 0;
+    var yValue = 10;
+    var newDataCount = 6;
+
+    var xValueBP = 0;
+    var yValue1 = 10;
+    var newDataCount1 = 6;
+
+    function addHeartRateData(data) {
+	    if(newDataCount != 1) {
+		    $.each(data, function(key, value) {
+			    HeartBeatPoints.push({x: value[0], y: parseInt(value[1])});
+			    averageheartrate.push({x:value[0], y:55});
+			    xValueHR++;
+			    yValue = parseInt(value[1]);
+		    });
+	    } else {
+		    //dataPoints.shift();
+		    HeartBeatPoints.push({x: data[0][0], y: parseInt(data[0][1])});
+		    averageheartrate.push({x:data[0][0], y:55});
+		    xValueHR++;
+		    yValue = parseInt(data[0][1]);
+	    }
+	
+	    newDataCount = 1;
+        var dataLength = 20;
+        if (HeartBeatPoints.length > dataLength) {
+		    HeartBeatPoints.shift();
+		    averageheartrate.shift();
+	    }
+	    heartRateChart.render();
+    
+	    setTimeout(updateHeartRateData, 2000);
+    }
+    function addBloodPressureData(data1) {
+	    if(newDataCount1 != 1) {
+		    $.each(data1, function(key, value) {
+			    systolicBP.push({x: value[0], y: parseInt(value[1])});
+                avgsystolicBP.push({x:value[0], y:120})
+                diastolicBP.push({x: value[0], y: parseInt(value[2])});
+                avgdiastolicBP.push({x:value[0], y:80})
+			    xValueBP++;
+			    yValue1 = parseInt(value[1]);
+		    });
+	    } else {
+		    systolicBP.push({x: data1[0][0], y: parseInt(data1[0][1])});
+            avgsystolicBP.push({x:data1[0][0], y:120})
+            diastolicBP.push({x: data1[0][0], y: parseInt(data1[0][2])});
+            avgdiastolicBP.push({x:data1[0][0], y:80})
+		    xValueBP++;
+		    yValue1 = parseInt(data1[0][1]);
+	    }
+	    var dataLength = 20;
+	    newDataCount1 = 1;
+        if (systolicBP.length > dataLength) {
+		    systolicBP.shift();
+            diastolicBP.shift();
+            avgdiastolicBP.shift();
+            avgsystolicBP.shift();
+	    }
+	    bloodpressureChart.render();
+	    setTimeout(updateBloodPressureData, 2000);
+}
+
+function updateHeartRateData() {
+    $.getJSON("http://localhost:8080/heartrate?xstart="+xValueHR+"&ambulanceID="+selectedAmbulance, addHeartRateData);
+}
+function updateBloodPressureData() {
+    $.getJSON("http://localhost:8080/bloodpressure?xstart="+xValueBP+"&ambulanceID="+selectedAmbulance, addBloodPressureData);
+}
+
+}
